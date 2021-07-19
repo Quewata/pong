63578fd41479438165983219a3120d8e63a45ccf
Quewata <87590631+Quewata@users.noreply.github.com>
2021-07-18 17:03:29 -0700

    Add files via upload

---
+++ pong.cpp
@@ -0,0 +1,85 @@
+//screen placement //########
+int(a, b == a) {
+    jmp(a - b == L) {
+        int(b - a == R) {
+            jmp(b - a == r) {
+
+            }
+        }
+    }
+}
+int(a + b = M) {
+
+}
+//functions //########
+int(a, b = 0,+1) {
+    jmp(b, a = 0,+1) {
+        int(b, a = 0,+1) {
+            jmp(a, b = 0,+1) {
+_loop() {
+
+}                
+            }
+        }
+    }
+}
+//player //########
+cin(a == Up) {
+    jmp(b == b) {
+        cin(b == Down)        
+    }
+}
+from cin(a == Up) {
+    jmp(b == b) {
+        cin(b == Down)        
+    }
+}
+int(a + b == ) {
+    int(a, b = 0,+1) {
+    jmp(b, a = 0,+1) {
+        int(b, a = 0,+1) {
+            jmp(a, b = 0,+1) {
+_loop() {
+
+}                
+            }
+        }
+    }
+}
+//score //########
+cin(a == Up) {
+    jmp(b == b) {
+        cin(b == Down)        
+    }
+}
+from cin(a == Up) {
+    jmp(b == b) {
+        cin(b == Down)        
+    }
+}
+int(a + b == ) {
+    int(a, b = 0,+1) {
+    jmp(b, a = 0,+1) {
+        int(b, a = 0,+1) {
+            jmp(a, b = 0,+1) {
+_loop() {
+
+}                
+            }
+        }
+    }
+}
+int(cin = 0,+1) {
+//win//lose //########
+if() {
+    cin(a == b == 10) {
+        cin(b == a == 10)
+        void(10,10 == a,b) {
+            more() {
+                done() {
+
+                }
+            }
+        }
+    }
+}
